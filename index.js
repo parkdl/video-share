5f92f177f3263a47daa3b9026c146852ffc0199e
PARKDL <42775456+PARKDL@users.noreply.github.com>
2019-01-11 22:17:42 +0900

    2-7 express middleware 2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -9,12 +13,11 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const bewteenHome = (req, res, next) => {
-  console.log("Bewteen");
-  next();
-};
-
-app.use(bewteenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.rulencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
