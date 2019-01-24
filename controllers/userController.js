b2195638193e3942568722fd449de3e04bfe17f0
PARKDL <42775456+PARKDL@users.noreply.github.com>
2019-01-24 18:13:36 +0900

    express 2-25 More controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,11 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("Logout", { pageTitle: "Log Out" });
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
+
 export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
