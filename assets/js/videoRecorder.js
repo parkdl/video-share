02747403b23060d69539ad848ffa6645065770e4
PARK D.I <ehddlf2395@gmail.com>
2019-02-20 23:42:47 +0900

    9.2 Recording video Part two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -5,16 +5,31 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
+
 const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -30,7 +45,7 @@ const getVideo = async () => {
   } catch (error) {
     recordBtn.innerHTML = "Cant record";
   } finally {
-    recordBtn.removeEventListener("click", startRecording);
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
