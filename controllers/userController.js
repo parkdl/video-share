b4678d7713de6d1796c1a9f87935b00a826c8909
PARKDL <ehddlf2395@gmail.com>
2019-10-04 14:11:30 +0900

    css edit and etc

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,8 +98,9 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const getMe = (req, res) => {
-  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+export const getMe = async (req, res) => {
+  const user = await User.findById(req.user.id).populate("videos");
+  res.render("userDetail", { pageTitle: "User Detail", user });
 };
 
 export const userDetail = async (req, res) => {
